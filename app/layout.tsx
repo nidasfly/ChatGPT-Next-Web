7ac83b6110c7154022d93de85baceac4c30caf5b
m46757sz <sihan.zeng@student.manchester.ac.uk>
2024-02-23 07:25:38 +0000

    personalized for tfyh

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT-天府银行",
+  description: "天府银行",
   viewport: {
     width: "device-width",
     initialScale: 1,
