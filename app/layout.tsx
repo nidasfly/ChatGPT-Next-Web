64cd1bb90eadfa889130dc9c35e0491bea300b7e
m46757sz <sihan.zeng@student.manchester.ac.uk>
2024-02-24 07:06:03 +0000

    personalized for hx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT-恒新线业",
+  description: "ChatGPT智能助手",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "恒新线业",
     statusBarStyle: "default",
   },
 };
